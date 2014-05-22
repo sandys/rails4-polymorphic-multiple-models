879bf3f5305c3def1f9b4d0cc35ca5f0b7e9d289
sandeep srinivasa <sss@lambdacurry.com>
2014-05-22 12:51:21 +0530

    updated keys

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,4 +1,4 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
   :server => 'email.us-east-1.amazonaws.com',
-  :access_key_id     => 'AKIAJ3AAWIDLHAGGXDWA',
-  :secret_access_key => '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'
+  :access_key_id     => 'AK',
+  :secret_access_key => 'Wr+vP'
